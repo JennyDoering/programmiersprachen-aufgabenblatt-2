8f6aa91760f429cb8e177a98d35e35a821e2f9c1
JennyDoering <jenny.doering@uni-weimar.de>
2019-04-17 17:28:24 +0200

    vec2.hpp bearbeitet (Aufgabe 2.2)

---
+++ source/vec2.hpp
@@ -0,0 +1,15 @@
+# ifndef VEC2_HPP
+# define VEC2_HPP
+
+// Vec2 data type definition
+
+struct Vec2
+{
+/* TODO add member variables with
+default member initialisation */
+
+    float x = 0.0f;
+    float y = 0.0f;
+
+};
+# endif // VEC2_HPP
