5094288cb5e29bfa83f98acdcb2eee8e6d9aea10
JennyDoering <jenny.doering@uni-weimar.de>
2019-04-17 17:27:02 +0200

    vec2.hpp in tests.cpp getestet (Aufgabe 2.2)

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,7 +1,24 @@
+#include "vec2.hpp"
+
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+
+
+TEST_CASE ( " vec2.hpp " , " [ vec2] " )
+{
+Vec2 a ; // requires that 0.0 f == a . x and 0.0 f == a . y
+Vec2 b {5.1f , -9.3f }; /* requires that 5.1 f == Approx ( a . y ) and -9.3 f == Approx ( b . y ) */
+
+REQUIRE(0.0f == a.x);
+REQUIRE(0.0f == a.y);
+
+REQUIRE(5.1f == Approx(b.x) );
+REQUIRE(-9.3f == Approx(b.y) ); 
+
+}
 
 int main(int argc, char *argv[])
 {
   return Catch::Session().run(argc, argv);
 }
+
