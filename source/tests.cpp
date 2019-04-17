9875e8a8f6d0f21e7d98c4a0156254a71c8cf16d
JennyDoering <jenny.doering@uni-weimar.de>
2019-04-17 18:12:35 +0200

    TEST CASES fuer Operatoren hinzugefuegt (Aufgabe 2.3)

--- source/tests.cpp
+++ source/tests.cpp
@@ -3,8 +3,8 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 
-
-TEST_CASE ( " vec2.hpp " , " [ vec2] " )
+//Aufgabe 2.2
+TEST_CASE ( " vec2.hpp " , " [vec2] " )
 {
 Vec2 a ; // requires that 0.0 f == a . x and 0.0 f == a . y
 Vec2 b {5.1f , -9.3f }; /* requires that 5.1 f == Approx ( a . y ) and -9.3 f == Approx ( b . y ) */
@@ -15,6 +15,42 @@ REQUIRE(0.0f == a.y);
 REQUIRE(5.1f == Approx(b.x) );
 REQUIRE(-9.3f == Approx(b.y) ); 
 
+}
+
+//Aufgabe 2.3 += operator
+TEST_CASE ( "operator +=" , "[vec2]" ) {
+  Vec2 f {4.5f, -1.9f};
+  Vec2 g {3.2f, 10.5f};
+  f+=g;
+  REQUIRE(7.7f == Approx(f.x));
+  REQUIRE(8.6f == Approx(f.y));
+}
+
+//Aufgabe 2.3 -= operator
+TEST_CASE ( "operator -=" , "[vec2]" ) {
+  Vec2 f {4.5f, -1.9f};
+  Vec2 g {3.2f, 10.5f};
+  f-=g;
+  REQUIRE(1.3f == Approx(f.x));
+  REQUIRE(-12.4f == Approx(f.y));
+}
+
+//Aufgabe 2.3 *= operator
+TEST_CASE ( "operator *=" , "[vec2]" ) {
+  Vec2 f {4.5f, -1.9f};
+  float s = 2;
+  f*=s;
+  REQUIRE(9.0f == Approx(f.x));
+  REQUIRE(-3.8f == Approx(f.y));
+}
+
+//Aufgabe 2.3 /= operator
+TEST_CASE ( "operator /=" , "[vec2]" ) {
+  Vec2 f {4.5f, -1.9f};
+  float s = 2;
+  f/=s;
+  REQUIRE(2.25f == Approx(f.x));
+  REQUIRE(-0.95f == Approx(f.y));
 }
 
 int main(int argc, char *argv[])
