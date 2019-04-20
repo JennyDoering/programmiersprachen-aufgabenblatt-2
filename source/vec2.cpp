2b7c2d50b5e48899b4429973c02d1748e78672b0
JennyDoering <jenny.doering@uni-weimar.de>
2019-04-20 18:34:18 +0200

    operatoren ueberarbeitet (Aufgabe 2.4) in vec2.cpp

--- source/vec2.cpp
+++ source/vec2.cpp
@@ -39,9 +39,7 @@ Vec2 operator-( Vec2 const& u , Vec2 const& v ) {
 }
 
 Vec2 operator*( Vec2 const& v , float s ) {
-    //float a = s;
-    //return v *= a;
-    //return v = v * s;
+   
     float b = s;
     Vec2 a = v * b;
     return a;
@@ -54,6 +52,8 @@ Vec2 operator/( Vec2 const& v , float s ) {
 }
 
 Vec2 operator*( float s , Vec2 const& v ){
+    
+    return v * s;
 
 }
 
