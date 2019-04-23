bba28812eaf450534d39f706fc3502cdfec1b8aa
JennyDoering <jenny.doering@uni-weimar.de>
2019-04-23 14:42:59 +0200

    mat2.cpp bearbeitet

--- source/mat2.cpp
+++ source/mat2.cpp
@@ -1,7 +1,14 @@
 #include "mat2.hpp"
 
 // Memberfunktion
-Mat2 & operator *=( Mat2 const & m ) {
+Mat2 & Mat2::operator *=( Mat2 const & m ) {
+   
+    // std::array (<std::array < float, 2 > , 2 > const & m);
+    // m = { 
+    //     {a, b}, 
+    //     {c, d};
+    // }
+   
     // float e_00 = (a* e + b* g);
     // float e_01 = (a* f + b* h);
     // float e_10 = (c* e + d* g);
@@ -12,12 +19,35 @@ Mat2 & operator *=( Mat2 const & m ) {
     // e_01 = (c.m * s + d.m * s);
     // e_11 = (c.m * s + d.m * s);
 
-    float s;
-    m := {e_00, e_01, e_10, e_11};
-    e_00 = (e_00.m * s + e_01.m * s);
-    e_01 = (e_00.m * s + e_01.m * s);
-    e_01 = (e_10.m * s + e_11.m * s);
-    e_11 = (e_10.m * s + e_11.m * s);
+    // float s;
+    // m := {e_00, e_01, e_10, e_11};
+    // e_00 = (e_00.m * s + e_01.m * s);
+    // e_01 = (e_00.m * s + e_01.m * s);
+    // e_01 = (e_10.m * s + e_11.m * s);
+    // e_11 = (e_10.m * s + e_11.m * s);
+
+    // e_00 = (e_00 * s + e_01 * s);
+    // e_01 = (e_00 * s + e_01 * s);
+    // e_01 = (e_10 * s + e_11 * s);
+    // e_11 = (e_10 * s + e_11 * s);
+
+    // e_00 = e_00 * s;
+    // e_01 = e_01 * s;
+    // e_10 = e_10 * s;
+    // e_11 = e_11 * s;
+
+//     for(int x=0;x<zeilen;x++){
+// 	for(int j=0;j<zeilen;j++){	
+// 		int result = 0;
+// 		for(int y=0;y<spalten;y++){			
+// 				result += matrixA[x][y] * matrixB[y][j];
+// 		}
+// 		matrixC[x][j] = result;
+// 	}		
+// }
+
+    
+
     return *this;
 }
 
