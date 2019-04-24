599e841c1d0e492e80be1ea5378666d7cd91106e
JennyDoering <jenny.doering@uni-weimar.de>
2019-04-24 12:44:47 +0200

    color.hpp hinzugefuegt (Aufgabe 2.7)

---
+++ source/color.hpp
@@ -0,0 +1,14 @@
+
+enum color {
+    r, g, b
+};
+
+struct color {
+    // mittlerer Grauton
+    float r = 0.5f;
+    float g = 0.5f;
+    float b = 0.5f;
+    // if (0 <= r <= 1 && 0 <= g <= 1 && 0 <= b <= 1) {
+
+    // }
+}
