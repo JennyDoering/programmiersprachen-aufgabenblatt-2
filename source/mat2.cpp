8b314dcd43e7e6f06f31542105c769cf9b8d60e5
JennyDoering <jenny.doering@uni-weimar.de>
2019-04-25 18:48:08 +0200

    operator mat*vec hinzugefuegt (Aufgabe 2.6)

--- source/mat2.cpp
+++ source/mat2.cpp
@@ -1,4 +1,5 @@
 #include "mat2.hpp"
+#include "vec2.hpp"
 
 // Memberfunktion (Aufgabe 2.5)
 Mat2 & Mat2::operator *=( Mat2 const & m ) {
@@ -32,3 +33,17 @@ float Mat2::det () const {
 
   return (a * c) - (b * d);
 }
+
+Vec2 operator*(Mat2 const & m, Vec2 const & v) {
+ 
+  float a = m.e_00 * v.x + m.e_01 * v.y;
+  float b = m.e_10 * v.x + m.e_11 * v.y;
+
+  Vec2 erg = {a, b};
+  return erg;
+}
+
+Vec2 operator*(Vec2 const & v, Mat2 const & m);
+Mat2 inverse(Mat2 const & m);
+Mat2 transpose(Mat2 const & m);
+Mat2 make_rotation_mat2(float phi);
