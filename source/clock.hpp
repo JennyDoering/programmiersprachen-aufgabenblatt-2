c01a849ddb8eedc8a99cbf4b28c8e2f34252d265
JennyDoering <jenny.doering@uni-weimar.de>
2019-04-30 19:54:45 +0200

    clock hinzugefuegt (Aufgabe 2.14)

---
+++ source/clock.hpp
@@ -0,0 +1,15 @@
+# ifndef CLOCK_HPP
+# define CLOCK_HPP
+# include "vec2.hpp"
+# include "mat2.hpp"
+# include "window.hpp"
+# include "circle.hpp"
+
+class Clock {
+
+public: 
+    Clock();
+    
+};
+
+# endif
